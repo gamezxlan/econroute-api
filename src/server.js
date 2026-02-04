5d1e21ce582bfeb83a72b538dad24e54ba730f11
gamezxlan <gallarzo.alberto@gmail.com>
2026-02-04 12:00:55 -0600

    Change console log message

--- src/server.js
+++ src/server.js
@@ -5,5 +5,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`EconRoute API running on http://localhost:${PORT}`);
+  console.log(`EconRoute API running on https://web-production-90baa.up.railway.app:${PORT}`);
 });
